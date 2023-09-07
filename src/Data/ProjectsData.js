009d1de486d34499eec2c38f7ebf3b1f6ff1c2a6
aadrsh26288 <78693934+aadrsh26288@users.noreply.github.com>
2023-09-07 21:25:15 +0530

    new projects

--- src/Data/ProjectsData.js
+++ src/Data/ProjectsData.js
@@ -1,66 +1,82 @@
-import TwinTech from '../Images/TwinTech.png'
-import WeatherApp from '../Images/WeatherApp.png'
-import Starbucks from '../Images/Starbucks.png'
-import Nft from '../Images/Nft.png'
+import TwinTech from "../Images/TwinTech.png";
+import WeatherApp from "../Images/WeatherApp.png";
+import Starbucks from "../Images/Starbucks.png";
+import Nft from "../Images/Nft.png";
 
-export const ProjectData =[
-    
-    {id:5,
-     Title:'ShopeO e-commerce',
-     Img:'https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8ZSUyMGNvbW1lcmNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60',
-     Description:'ShopeO e-commence website built using React, Redux Toolkit, and TailwindCSS. which consist functionality like add cart and advance products filter ',
-     Github:'https://github.com/aadrsh26288/ShopO-e-commerce',
-     Link:'https://shop-o-e-commerce.vercel.app/',
-     Skills:['HTML','TailwindCSS','JS','React','Redux-Toolkit']
-    },
-   {id:4,
-     Title:'Chat App',
-     Img:'https://images.pexels.com/photos/5053740/pexels-photo-5053740.jpeg?auto=compress&cs=tinysrgb&w=600',
-     Description:' This project provides the instant communication functionality between users only authenticated user can access the services ',
-     Github:'https://github.com/aadrsh26288/ChatApp',
-     Link:'https://chat-app-gamma-three.vercel.app/',
-     Skills:['HTML','TailwindCSS','JS','React','Firebase']
-    },   
+export const ProjectData = [
+	{
+		id: 7,
+		Title: "The Pen's Point ",
+		Img: "https://images.pexels.com/photos/1591056/pexels-photo-1591056.jpeg?auto=compress&cs=tinysrgb&w=600",
+		Description:
+			"The Pen's Point is a blog website created using React, Tailwind CSS, and Firebase. It offers functionality that allows users to create their own blogs, comment on posts, and like other blogs, but only if they are authenticated ",
+		Github: "",
+		Link: "https://blog-app-lilac-one.vercel.app/article/AV9bARLRKbeaN6h4j2Bs",
+		Skills: ["HTML", "TailwindCss", "React Js", "Firebase"],
+	},
 
-        {id:2,
-     Title:'Starbucks 2.0',
-     Img:"https://images.unsplash.com/photo-1637290742802-3c8a0f5bd49b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHN0YXJidWNrc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
-     Description:'Starbucks 2.0 is a clone landing page that employs Redux for AddCart functionality and Tailwind CSS for styling.',
-     Github:'https://github.com/aadrsh26288/Starbucks-2.0',
-     Link:'https://starbucks-2-0.vercel.app/',
-     Skills:['HTML','Tailwind CSS','JS','React','Redux']
-    },
+	{
+		id: 6,
+		Title: "ShopeO e-commerce",
+		Img: "https://images.unsplash.com/photo-1661956602944-249bcd04b63f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxzZWFyY2h8MXx8ZSUyMGNvbW1lcmNlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60",
+		Description:
+			"ShopeO e-commence website built using React, Redux Toolkit, and TailwindCSS. which consist functionality like add cart and advance products filter ",
+		Github: "https://github.com/aadrsh26288/ShopO-e-commerce",
+		Link: "https://shop-o-e-commerce.vercel.app/",
+		Skills: ["HTML", "TailwindCSS", "JS", "React", "Redux-Toolkit"],
+	},
+	{
+		id: 4,
+		Title: "Chat App",
+		Img: "https://images.pexels.com/photos/5053740/pexels-photo-5053740.jpeg?auto=compress&cs=tinysrgb&w=600",
+		Description:
+			" This project provides the instant communication functionality between users only authenticated user can access the services ",
+		Github: "https://github.com/aadrsh26288/ChatApp",
+		Link: "https://chat-app-gamma-three.vercel.app/",
+		Skills: ["HTML", "TailwindCSS", "JS", "React", "Firebase"],
+	},
 
-    {id:1,
-     Title:'Twin Tech',
-     Img:TwinTech,
-     Description:'Twin Tech is a landing page that uses Firebase as a database for storing blog and store data.',
-     Github:'https://github.com/aadrsh26288/TwinTechh',
-     Link:'https://twintechhh.vercel.app/',
-     Skills:['HTML','Tailwind CSS','JS','React','Firebase',]
-    },
+	{
+		id: 2,
+		Title: "Starbucks 2.0",
+		Img: "https://images.unsplash.com/photo-1637290742802-3c8a0f5bd49b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fHN0YXJidWNrc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+		Description:
+			"Starbucks 2.0 is a clone landing page that employs Redux for AddCart functionality and Tailwind CSS for styling.",
+		Github: "https://github.com/aadrsh26288/Starbucks-2.0",
+		Link: "https://starbucks-2-0.vercel.app/",
+		Skills: ["HTML", "Tailwind CSS", "JS", "React", "Redux"],
+	},
 
+	{
+		id: 1,
+		Title: "Twin Tech",
+		Img: TwinTech,
+		Description:
+			"Twin Tech is a landing page that uses Firebase as a database for storing blog and store data.",
+		Github: "https://github.com/aadrsh26288/TwinTechh",
+		Link: "https://twintechhh.vercel.app/",
+		Skills: ["HTML", "Tailwind CSS", "JS", "React", "Firebase"],
+	},
 
+	{
+		id: 3,
+		Title: "Weather App",
+		Img: WeatherApp,
+		Description:
+			"It gives the information about the current weather you just need to enter the city name you want to check",
+		Github: "",
+		Link: "https://snazzy-tapioca-52befe.netlify.app/",
+		Skills: ["HTML", "CSS", "JS"],
+	},
 
-    {id:3,
-     Title:'Weather App',
-     Img:WeatherApp,
-     Description:'It gives the information about the current weather you just need to enter the city name you want to check',
-     Github:'',
-     Link:'https://snazzy-tapioca-52befe.netlify.app/',
-     Skills:['HTML','CSS','JS']
-    },
-
- 
-
-    {id:5,
-     Title:'Nft Marketplace',
-     Img:Nft,
-     Description:'Nft Marketplace template is Collection Nfts which uses React ,TailwindCss, Firebase ',
-     Github:'https://github.com/aadrsh26288/My_Nft',
-     Link:'https://my-nft-two.vercel.app/',
-     Skills:['HTML','CSS','JS']
-    },
-
-
-]
+	{
+		id: 5,
+		Title: "Nft Marketplace",
+		Img: Nft,
+		Description:
+			"Nft Marketplace template is Collection Nfts which uses React ,TailwindCss, Firebase ",
+		Github: "https://github.com/aadrsh26288/My_Nft",
+		Link: "https://my-nft-two.vercel.app/",
+		Skills: ["HTML", "CSS", "JS"],
+	},
+];
